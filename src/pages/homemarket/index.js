261cabb13d16fc265f09c6ddfa3a90550c53a2dc
oliveirajulio <juliocesargomes33@gmail.com>
2024-12-15 11:14:17 -0300

    domingo

--- src/pages/homemarket/index.js
+++ src/pages/homemarket/index.js
@@ -16,8 +16,9 @@ function homemarket() {
                     <ul>
                         <li onClick={granel}>Granel</li>
                         <li>Mercearia</li>
+                        <li>Oficinais</li>
+                        <li>Oleos Variados</li>
                         <li>Suplementos</li>
-                        <li>Hortifruiti</li>
                         <li>Vestuário</li>
                     </ul>
                 </nav>
@@ -28,14 +29,42 @@ function homemarket() {
                 </h1>
             </div>
             <nav className="nav2">
-                    <ul>
-                        <p className="item">Granel</p>
-                        <p className="item">Mercearia</p>
-                        <p className="item">Suplementos</p>
-                        <p className="item">Hortifruiti</p>
-                        <p className="item">Vestuário</p>
-                    </ul>
-                </nav>
+                <ul>
+                    <p onClick={granel}>Granel</p>
+                    <p>Mercearia</p>
+                    <p>Oficinais</p>
+                    <p>Oleos Variados</p>
+                    <p>Suplementos</p>
+                    <p>Vestuário</p>
+                </ul>
+            </nav>
+            <div className="ctn-dynamic">
+                <div className="nav-move">
+                    <div className="it1">
+                        <img src="/imagens/maca_peruana.png" alt="Descrição da imagem" />
+                        <h4>Maca Peruana</h4>
+
+
+                    </div>
+                    <div className="it2">
+                        <img src='/imagens/moringa.jpg' alt="moringa" />
+                        <h4>Moringa</h4>
+                        
+
+                    </div>
+                    <div className="it3">
+                        <img src='/imagens/curcuma.jpg' alt="moringa" />
+                        <h4>Cúrcuma</h4>
+
+                        
+                    </div>
+                    <div className="it4">
+                        <img src='/imagens/farinha_de_uva.jpg' alt="moringa" />
+                        <h4>Farinha de Uva</h4>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
